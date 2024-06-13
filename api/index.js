d4af7226c25337b9a93ab8aaa2358fbdf0234990
Cesar <cesar.alcoholado@hotmail.com>
2024-06-13 12:18:02 -0300

    fixing cors issue

--- api/index.js
+++ api/index.js
@@ -6,8 +6,22 @@ import { findDissimilarColor, isDarkColor, rgbToHex } from './utils/helpers.js';
 const app = express();
 app.use(cors());
 
-app.get("/", (req,res) => {
-  res.send("hello world")
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://dynamic-presentation-card-6bjh.vercel.app/"
+  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
 app.get("/card", async (req, res) => {
