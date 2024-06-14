72de737f403dbcca6b3e08d1e0c718cec4e7f89e
Cesar <cesar.alcoholado@hotmail.com>
2024-06-14 11:45:46 -0300

    fixing deployment issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const getCardInfo = async () => {
     try {
       const response = await fetch(
-        `https://dynamic-presentation-card.vercel.app/card?domain=${domain}`
+        `https://dynamic-presentation-card.vercel.app/card?domain=${domain}`,{mode: "no-cors"}
       );
       const data = await response.json();
       setCardInfo(data);
